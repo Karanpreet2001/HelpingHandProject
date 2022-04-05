ad2bfc5fdd1febaf6ab456b87a2d8b0e87134dc8
Sahil Sharma <sahil.sharma071997@gmail.com>
2022-04-05 10:49:25 -0700

    fixed login

--- client/src/components/signin.jsx
+++ client/src/components/signin.jsx
@@ -1,87 +1,61 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import {useNavigate} from "react-router-dom";
-import { useLocation } from 'react-router';
-
-
 
 const SignIn = ({contact}) => {
 
-
-    const [data,setData]= useState([]);
-
-    useEffect(()=>{
-        axios.get("http://localhost:5000/api/Login")
-        .then(resp=>{
-          console.log(resp,resp.data);
-          setData(resp.data);
-          
-        })
-        .catch(err=>{
-          console.log(err);
-        })
-      },[]);
-
-
+    const [username, setUsername]=useState("");
+    const [password, setPassword]=useState("");
+    const [type, setType]=useState("C");
     const navigate= useNavigate();
-    const [s, setS]=useState({});
 
-  
     const handleChange=(e)=>{
-
-
         const target = e.target;
-  
-
-
         if(target.id==="inputEmail"){
-           s.username=target.value;
+            setUsername(target.value);
         }
         if(target.id==="inputPassword"){
-           s.password=target.value
+           setPassword(target.value);
         }
-
         if(target.id==="type"){   
-            s.type="SP";
-        }else{    
-            s.type="C";
-        } 
-
+            if(type === "SP"){
+                setType("C");
+            } else {
+                setType("SP");
+            }
+        }
     }
-    console.log(s);
-
     
-
      const checkUser=  (e)=>{
         e.preventDefault();
 
-        
+        axios.get("http://localhost:5000/api/Login")
+        .then(resp=>{
             
-             
-        for(var i=0; i<data.length;i++){
+            let data = resp.data;
+            for(var i=0; i<data.length;i++){
+                console.log("username ", username, " password ", password, " type ", type);
 
-            if(s.username !==""& s.password !==""){
-
-                if(data[i].username===s.username && data[i].password===s.password && data[i].type===s.type){
-
-                  contact = s.password;
-                    if(s.type==="SP"){
-                        
-                        navigate("/serviceProvider" , {state: s.username});
-                    }else{
-                        navigate("/home",{state: s.password});
+                if(username !=="" && password !==""){
+                    console.log(data[i].username)
+                    if(data[i].username===username && data[i].password===password && data[i].type===type){
+                      contact = password;
+                      
+                        if(type==="SP"){
+                            navigate("/serviceProvider" , {state: username});
+                        }else{
+                            navigate("/home",{state: password});
+                        }
                     }
+                }else{
+                    navigate("/error");
                 }
-            }else{
-                navigate("/error");
-            }
-           
-        }    
-
+            }            
+        })
+        .catch(err=>{
+          console.log(err);
+        });   
     }
-
-
-    
 
     return ( <> <div className="d-flex justify-content-center align-items-center container ">
     <form>
@@ -89,17 +63,17 @@ const SignIn = ({contact}) => {
         <div className="row mb-3">
             <label htmlFor="inputEmail" className="col-sm-2 col-form-label mx-3">Email</label>
             <div className="col-sm-10">
-                <input type="text" className="form-control" name="inputEmail" id="inputEmail" value={s.username} placeholder="Email" onChange={handleChange} />
+                <input type="text" className="form-control" name="inputEmail" id="inputEmail" value={username} placeholder="Email" onChange={handleChange} />
             </div>
         </div>
         <div className="row mb-3">
             <label htmlFor="inputPassword" className="col-sm-3 col-form-label mx-3">Password</label>
             <div className="col-sm-10">
-                <input type="password" className="form-control" name="inputPassword" id="inputPassword" value={s.password} placeholder="Password" onChange={handleChange} />
+                <input type="password" className="form-control" name="inputPassword" id="inputPassword" value={password} placeholder="Password" onChange={handleChange} />
             </div>
         </div>
         <div className="form-check">
-         <input className="form-check-input" type="checkbox" value={s.type} id="type" onChange={handleChange}/>
+         <input className="form-check-input" type="checkbox" value={type} id="type" onChange={handleChange}/>
          <label className="form-check-label" htmlFor="flexCheckDefault">
           Service Provider
           </label>
